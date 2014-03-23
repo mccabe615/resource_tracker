0aa86e4440be7568c9f43b82627111301ce10665
Prelang Builder <builder@prelang.com>
2014-03-23 19:22:44 +0000

    Initial commit; clean Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ResourceTracker::Application.config.secret_key_base = '3e9a6066ae4799e37d5c276e33ef3c0e42998391b11688b9ee5c7350498d85ae63c84d5f103a2ea2c0b1f05b3b407f1030b9483187de8cf6ef6e96fb230e91e4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ResourceTracker::Application.config.session_store :cookie_store, key: '_resource_tracker_session'
